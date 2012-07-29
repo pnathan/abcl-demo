9721828dab4fdd19f2579e142e4217f4d26c1cb8
pdn <pnathan@vandals.uidaho.edu>
2012-07-29 10:37:46 -0700

    Cleanup

--- lispRunner.java
+++ lispRunner.java
@@ -10,24 +10,21 @@ class lispRunner
    {
       try
       {
-          //
           Interpreter interpreter = Interpreter.createInstance();
           interpreter.eval("(load \"lispfunction.lisp\")");
 
           // Get a datapoint
           LispObject one_hundred = JavaObject.getInstance(100, true);
-          org.armedbear.lisp.Package defaultPackage =
-           Packages.findPackage("CL-USER");
-          Symbol sym =
-           defaultPackage.findAccessibleSymbol("LISPFUNCTION");
+          Package defaultPackage = Packages.findPackage("CL-USER");
+          Symbol sym = defaultPackage.findAccessibleSymbol("LISPFUNCTION");
           Function function = (Function) sym.getSymbolFunction();
           LispObject result = function.execute(one_hundred);
           System.out.print(result.getStringValue());
       }
-      catch (Throwable t)
+      catch (Throwable exception)
       {
-              System.out.println("exception!");
-       t.printStackTrace();
+              System.out.println("Error Occurred in ABCL!");
+              exception.printStackTrace();
       }
    }
 }
